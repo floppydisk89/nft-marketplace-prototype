155de847b8f9ece714c37b8b67b145f87f49f306
floppydisk89 <53581002+floppydisk89@users.noreply.github.com>
2022-09-07 14:04:40 +0200

    Refractored code.

--- src/components/NftGrid/index.jsx
+++ src/components/NftGrid/index.jsx
@@ -12,7 +12,7 @@ export default function NftGrid() {
   const content = data.marketplace.nft_grid;
 
   async function mintSelectedNft(nftId) {
-    if (sessionState.ownedNfts.indexOf(nftId) > -1 === true) return;
+    if (sessionState.ownedNfts.includes(nftId)) return;
 
     if (sessionState.walletAddress) {
       const newMintedNft = await mintNft(
@@ -29,28 +29,27 @@ export default function NftGrid() {
   return (
     <div className='nft-grid-container'>
       <div className='grid'>
-        {content.length &&
-          content.map((nft, key) => {
-            return (
-              <div className='card' key={key}>
-                <img className='nft-image' src={nft.image} alt='' />
-                <div className='nft-about'>
-                  <div className='about'>
-                    <h2 className='title'>#{nft.title}</h2>
-                    <p className='author'>{nft.author}</p>
-                  </div>
-                  <span
-                    className='button'
-                    onClick={() => mintSelectedNft(nft.title)}
-                  >
-                    {sessionState.ownedNfts.indexOf(nft.title) > -1
-                      ? 'Minted'
-                      : 'Mint'}
-                  </span>
+        {content.map((nft) => {
+          return (
+            <div className='card' key={nft.id}>
+              <img className='nft-image' src={nft.image} alt='' />
+              <div className='nft-about'>
+                <div className='about'>
+                  <h2 className='title'>#{nft.title}</h2>
+                  <p className='author'>{nft.author}</p>
                 </div>
+                <span
+                  className='button'
+                  onClick={() => mintSelectedNft(nft.title)}
+                >
+                  {sessionState.ownedNfts.includes(nft.title)
+                    ? 'Minted'
+                    : 'Mint'}
+                </span>
               </div>
-            );
-          })}
+            </div>
+          );
+        })}
       </div>
     </div>
   );
