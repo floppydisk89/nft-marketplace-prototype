6b0c6e8fe73150336be726c295947d8e0b59245b
floppydisk89 <53581002+floppydisk89@users.noreply.github.com>
2022-09-07 11:50:59 +0200

    Removed unused imports and test code.

--- src/components/NftGrid/index.jsx
+++ src/components/NftGrid/index.jsx
@@ -1,33 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import {
-  setWalletAddress,
-  setNetworkId,
-  setOwnedNfts,
-  setEthereumContract,
-} from '../../store/sessionStateSlice';
-import { loadWeb3 } from '../../services/web3.service';
 import { mintNft } from '../../services/web3.service';
-import { walletConnector } from '../../services/walletConnector.service';
 
 import './index.scss';
 import data from '../../data/content.json';
 
 export default function NftGrid() {
   const [content, setContent] = useState({});
-  const [web3Provider, setWeb3Provider] = useState(null);
 
   const dispatch = useDispatch();
   const sessionState = useSelector((state) => state.sessionState);
 
   useEffect(() => {
     setContent(data.marketplace.nft_grid);
-
-    loadWeb3().then((provider) => {
-      setWeb3Provider(provider);
-    });
-
-    console.log('render', sessionState);
   }, []);
 
   function mintSelectedNft(nftId) {
